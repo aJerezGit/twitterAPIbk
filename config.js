2e01dd4efe15353a46c88af1962a899f6046a50d
Ajerez-Pyosoft <andresjerezg@hotmail.com>
2020-06-01 00:14:19 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const Twitter = require('twit');
 const port = process.env.PORT || 3000;
 const apiClient = new Twitter({
-    consumer_key: 'ian4MxjNmkBz3GccPMABeE7pn',
-    consumer_secret: 'U8VsHJD66VcRN9QS7iVxcBLlSlNIVLD6md5t8snYCDQHf9EQc7',
-    access_token: '1266450555632791558-fwU3GT37brSSZTrJNms3Aa8TpMImgm',
-    access_token_secret: 'GkFVX5aG3ViHEUbuqeRA9srwqudC4WRup0pwq8vK6bclU'
+    consumer_key: 'your_consumer_key',
+    consumer_secret: 'your_consumer_secret',
+    access_token: 'your_access_token',
+    access_token_secret: 'your_acces_token_secret'
 });
 
 module.exports = { port, apiClient };
